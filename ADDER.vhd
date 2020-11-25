638dcbc16186c09e5d8a323f18abc4ab1fb445d1
Alexandre Genthner <genthner.ah@gmail.com>
2020-11-24 22:54:55 -0300

    Update ADDER.vhd

--- ADDER.vhd
+++ ADDER.vhd
@@ -4,21 +4,16 @@ use ieee.std_logic_unsigned.all;
 use ieee.std_logic_arith.all;
 
 ENTITY ADDER IS
-	port(	signal_a : in std_logic_vector(7 downto 0); 
-		signal_b : in std_logic_vector(15 downto 0); 
+	port(	signal_a : in std_logic_vector(7 downto 0);
+		signal_b : in std_logic_vector(15 downto 0);
 	  	signal_rsl : out std_logic_vector(15 downto 0)
 	  	);
 END ADDER;
 
 architecture behavior of ADDER is
 
-	--signal AUX : std_logic_vector(15 downto 0);
+begin
 
-begin 
-	process(signal_b)
-
-		begin
 		signal_rsl <= ("00000000" & signal_a) + signal_b;
-	end process;
 
 end behavior;
