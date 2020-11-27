09c08cbb8024514948756ee66f0ce48f0bbc380a
KybiBytemann <genthner.ah@gmail.com>
2020-11-27 20:15:51 -0300

    COnsideração para a multiplicação a mais que era feita

--- COMPR.vhd
+++ COMPR.vhd
@@ -4,20 +4,31 @@ use ieee.std_logic_unsigned.all;
 --use ieee.std_logic_arith.all;
 
 ENTITY COMPR IS
-	port(	signal_x : in std_logic_vector(7 downto 0); 
-		signal_y : in std_logic_vector(7 downto 0); 
-	  	control_c : out std_logic
-	  	);
+	port(
+	 signal_x : in std_logic_vector(7 downto 0);
+	 signal_y : in std_logic_vector(7 downto 0);
+	 control_c : out std_logic
+	);
 END COMPR;
 
 architecture behavior of COMPR is
 
-	--signal AUX : std_logic_vector(15 downto 0);
+	signal signal_compr : std_logic_vector(7 downto 0);
 
-begin 
+begin
+
 	process(signal_x)
+
 	begin
-		if signal_x /= signal_y then
+		-- deve-se diminuir de uma unidade para que a opera
+		-- cao ocorra normalmente
+		if signal_y = "00000000" then
+			signal_compr <= signal_y;
+		else
+			signal_compr <= signal_y + "11111111";
+		end if;
+
+		if signal_x /= signal_compr then
 			control_c <= '1';
 		else
 			control_c <= '0';
