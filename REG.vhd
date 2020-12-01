3c49d48ab29e0362eba9dd89eab86b2e01d8e371
Andre Mattos <andrempmattos@gmail.com>
2020-12-01 19:29:54 -0300

    reg: Melhorando sincronização

--- REG.vhd
+++ REG.vhd
@@ -1,29 +1,31 @@
-library ieee ;
-use ieee.std_logic_1164.all ;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity REG is
 
-	port(	signal_r 	: in std_logic_vector(15 downto 0) ;
-		control_clk	: in std_logic ;
-		control_rst	: in std_logic ;
-		control_en	: in std_logic ;
+	port(	
+		signal_r 	: in std_logic_vector(15 downto 0);
+		control_clk	: in std_logic;
+		control_rst	: in std_logic;
+		control_en	: in std_logic;
 		signal_q 	: out std_logic_vector(15 downto 0) 
-		);
+	);
 end REG;
 
 architecture behavior of REG is
 
 	begin
-		process
-			begin
-			wait until control_clk'EVENT and control_clk = '1' ;
-
-			if control_rst = '1' then
-				signal_q <= "0000000000000000";
-			else
-				if control_en = '1' then signal_q <= signal_r ;
-				end if;
-			end if;
+		process(control_rst, control_clk)
+    	begin
+	        if (control_rst = '1') then
+	            signal_q <= (others=>'0');
+	        elsif (rising_edge(control_clk)) then
+	            signal_q <= signal_r;
+	    	end if;
 		end process;
 
 end behavior ;
+
+
+
+	
