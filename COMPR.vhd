91e453a608722713937ab7169d1a1b3a01e31ad0
Andre Mattos <andrempmattos@gmail.com>
2020-12-01 19:26:56 -0300

    compr: Atualizando a implementação de sequencial para concorrente

--- COMPR.vhd
+++ COMPR.vhd
@@ -1,27 +1,18 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.std_logic_unsigned.all;
---use ieee.std_logic_arith.all;
 
 ENTITY COMPR IS
-	port(	signal_x : in std_logic_vector(7 downto 0); 
-		signal_y : in std_logic_vector(7 downto 0); 
+	port(	
+		signal_x  : in std_logic_vector(7 downto 0); 
+		signal_y  : in std_logic_vector(7 downto 0); 
 	  	control_c : out std_logic
-	  	);
+	);
 END COMPR;
 
 architecture behavior of COMPR is
 
-	--signal AUX : std_logic_vector(15 downto 0);
-
-begin 
-	process(signal_x)
-	begin
-		if signal_x /= signal_y then
-			control_c <= '1';
-		else
-			control_c <= '0';
-		end if;
-	end process;
+	begin 
+		control_c <= "0" when (signal_x < signal_y) else "1";
 
 end behavior;
+		
