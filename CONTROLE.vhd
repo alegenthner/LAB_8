0328dcbda732ccf5936d0c098a5f482c4cf67278
Andre Mattos <andrempmattos@gmail.com>
2020-12-01 19:28:06 -0300

    controle: Atualizando a implementação para diferentes processos e melhorando sincronização

--- CONTROLE.vhd
+++ CONTROLE.vhd
@@ -1,47 +1,72 @@
-library ieee ;
-use ieee.std_logic_1164.all ;
---use ieee.std_logic_arith.all;
-use ieee.std_logic_unsigned.all;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity CONTROLE is
 
-	port(	control_clk	: in std_logic ;
-		control_start	: in std_logic ;
-		control_c0 	: in std_logic ;
-		control_ena	: out std_logic ;
-		control_rst	: out std_logic ;
-		control_busy	: out std_logic
-		);
+	port(	
+		control_clk	  : in std_logic;
+		control_start : in std_logic;
+		control_c0 	  : in std_logic;
+		control_ena	  : out std_logic;
+		control_rst	  : out std_logic;
+		control_busy  : out std_logic
+	);
 end CONTROLE;
 
 architecture behavior of CONTROLE is
 
 type state_type is (S0,S1,S2);
-signal q : state_type;
+signal current_state : state_type;
+signal next_state : state_type;
 
 	begin
-		process(control_clk)
-			begin
-			--control_clk'EVENT and control_clk = '1' ;
-
-			case q is 
-			when S0 => 
-				if control_start = '0'
-				then  q <= S0;
-				else q <= S1;
-				end if;
-			when S1 =>
-				q <= S2;
-			when S2 =>
-				if control_c0 = '1'
-				then q <= S2;
-				else q <= s0;
-				end if;
+		-- Next state logic
+		process(current_state)
+		begin
+			next_state <= current_state;
+			case current_state is
+				when S0 =>
+					if control_start = '1' then
+						next_state <= S1;
+					end if;
+				when S1 => 
+					next_state <= S2;
+				when S2 =>
+					if control_c0 = '0' then
+						next_state <= S0;
+					end if;
 			end case;
 		end process;
 
-	control_ena <= '1' when q = S2 else '0';
-	control_rst <= '1' when q = S1 else '0';
-	control_busy <= '0' when q <= S0 else '1';
+		-- State register
+		process(control_clk, control_rst)
+		begin
+			if Reset = '1' then 
+				current_state <= S0;
+			elsif rising_edge(Clock) then
+				current_state <= next_state; 
+			end if;
+		end process;
+
+		-- Output logic
+		process(current_state)
+		begin
+			control_busy <= '1';
+			control_rst  <= '0';
+			control_ena  <= '0';
+			
+			case current_state is
+				when S0 => 
+					control_busy <= '0';
+				when S1 =>
+					control_rst  <= '1';
+				when S2 =>
+					control_ena  <= '1';
+			end case;
+		end process;
 
 end behavior ;
+
+
+
+	
