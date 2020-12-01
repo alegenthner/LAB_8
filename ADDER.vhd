c10a685c2afb421757de2fd7df4fb01b3561417f
Andre Mattos <andrempmattos@gmail.com>
2020-12-01 19:24:58 -0300

    adder: Melhorando indentação

--- ADDER.vhd
+++ ADDER.vhd
@@ -1,19 +1,18 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.std_logic_unsigned.all;
 use ieee.std_logic_arith.all;
 
 ENTITY ADDER IS
-	port(	signal_a : in std_logic_vector(7 downto 0);
-		signal_b : in std_logic_vector(15 downto 0);
+	port(	
+		signal_a   : in std_logic_vector(7 downto 0);
+		signal_b   : in std_logic_vector(15 downto 0);
 	  	signal_rsl : out std_logic_vector(15 downto 0)
-	  	);
+	);
 END ADDER;
 
 architecture behavior of ADDER is
 
-begin
-
+	begin
 		signal_rsl <= ("00000000" & signal_a) + signal_b;
 
 end behavior;
